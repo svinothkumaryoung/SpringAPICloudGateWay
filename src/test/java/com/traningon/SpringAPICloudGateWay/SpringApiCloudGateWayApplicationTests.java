cf1b5ba81bdf06ea9c2fecdd2170105ff9240e2d
svinothkumaryoung <vinchi.frnds@gmail.com>
2022-10-20 11:34:15 +0530

    Second MicroServices

---
+++ src/test/java/com/traningon/SpringAPICloudGateWay/SpringApiCloudGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traningon.SpringAPICloudGateWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiCloudGateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
